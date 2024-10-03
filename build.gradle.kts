d4207747723310a82668d9701ad7928c2b476db7
raichel sunny <raichel9988@gmail.com>
2024-10-03 23:12:32 +0530

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules. buildscript {
+
+   buildscript {
+       dependencies {
+
+           "com.google.gms:google-services:4.4.2"
+       }
+   }
+    plugins {
+         "com.android.application" version "7.4.2" apply false
+        "com.android.library" version "7.4.2" apply false
+    }
+
+
+
+
+
+
+
+
